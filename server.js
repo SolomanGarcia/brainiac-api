f0759c1df56730236594d74e43f105bafaec73c8
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-21 15:10:41 -0700

    install bcrypt

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
 
 const app = express();
 
@@ -54,6 +55,9 @@ app.post("/guest", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
+  bcrypt.hash(password, null, null, function (err, hash) {
+    console.log(hash);
+  });
   database.users.push({
     id: "125",
     name: name,
