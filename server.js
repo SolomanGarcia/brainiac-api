f3ccfd39b047ed94c575a379835ce64dcf4b7062
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-25 07:46:46 -0700

    work on profile endpoint

--- server.js
+++ server.js
@@ -87,16 +87,19 @@ app.post("/register", (req, res) => {
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach((users) => {
-    if (users.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(400).json("not found");
-  }
+  db.select("*")
+    .from("users")
+    .where({
+      id
+    })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("Not found");
+      }
+    })
+    .catch((err) => res.status(400).json("Error getting user"));
 });
 
 app.put("/image", (req, res) => {
