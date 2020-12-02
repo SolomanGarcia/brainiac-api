78ca32fd912d38b7edabd63872da684a7b70d6b6
Soloman Garcia <soloman.garcia@gmail.com>
2020-12-02 08:31:49 -0700

    update db

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-asymmetrical-74939",
     user: "postgres",
     password: "test",
     database: "brainiac"
